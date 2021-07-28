fc02c90708e0fcb943682a9451a0b79f485da219
LeLoomi <lohr.eliah@gmail.com>
2021-07-28 20:50:17 +0200

    Projektdateien hinzufügen.

---
+++ DynamicShortcuts/mainForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace DynamicShortcutsTheory
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.createButton = new System.Windows.Forms.Button();
+            this.readButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // createButton
+            // 
+            this.createButton.Location = new System.Drawing.Point(34, 31);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(126, 23);
+            this.createButton.TabIndex = 0;
+            this.createButton.Text = "create shortcut";
+            this.createButton.UseVisualStyleBackColor = true;
+            this.createButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // readButton
+            // 
+            this.readButton.Location = new System.Drawing.Point(34, 89);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(126, 23);
+            this.readButton.TabIndex = 1;
+            this.readButton.Text = "read shortcut";
+            this.readButton.UseVisualStyleBackColor = true;
+            this.readButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(34, 60);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(126, 23);
+            this.updateButton.TabIndex = 2;
+            this.updateButton.Text = "update shortcut";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(232, 63);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(38, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(170, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Icon index";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 147);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.readButton);
+            this.Controls.Add(this.createButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "DynamicIcons";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.Button readButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DynamicShortcuts/mainForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace DynamicShortcutsTheory
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            createNewIcon("iCloud Calendar");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            readLinkToFile("iCloud Calendar");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            updateIcon("iCloud Calendar", numericUpDown1.Value.ToString());
+        }
+
+
+        //Create init icon
+        private void createNewIcon(string shortcutName)
+        {
+            string deskDir = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+            using (StreamWriter writer = new StreamWriter(deskDir + "\\" + shortcutName + ".url"))
+            {
+                writer.WriteLine("[InternetShortcut]");
+                writer.WriteLine("URL=https://www.icloud.com/calendar/");
+                writer.WriteLine("IconIndex=0");
+                string icon = @"C:\Users\lohre\Code Projects\DynamicShortcutsTheory\DynamicShortcutsTheory\icons\" + numericUpDown1.Value.ToString() + ".ico";
+                writer.WriteLine("IconFile=" + icon);
+                writer.WriteLine("HotKey=0");
+                writer.WriteLine("IDList =");
+                writer.Close();
+            }
+        }
+
+        private void updateIcon(string shortcutName, string iconIndex)
+        {
+            string deskDir = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            string newIconPath = @"IconFile=C:\Users\lohre\Desktop\Dynamic Shortcuts\icons\" + iconIndex + ".ico";
+
+            lineChanger(newIconPath, deskDir + "\\" + shortcutName + ".url", 4);
+
+            System.Diagnostics.Process.Start(AppDomain.CurrentDomain.BaseDirectory + @"\clearIcons.cmd");
+            readLinkToFile("iCloud Calendar");
+        }
+
+        //read icon to text file
+        private void readLinkToFile(string shortcutName)
+        {
+            string deskDir = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+            using (StreamReader reader = new StreamReader(deskDir + "\\" + shortcutName + ".url"))
+            {
+                string content = reader.ReadToEnd();
+
+                StreamWriter writer = new StreamWriter(deskDir + "\\" + shortcutName + ".log");
+                writer.WriteLine(content);
+                writer.Close();
+            }
+
+        }
+
+        static void lineChanger(string newText, string fileName, int line_to_edit)
+        {
+            string[] arrLine = File.ReadAllLines(fileName);
+            arrLine[line_to_edit - 1] = newText;
+            File.WriteAllLines(fileName, arrLine);
+        }
+    }
+}
